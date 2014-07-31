2811ff085f5eb8dcd8f8b567efa7948862cdd801
Nunki <sprimont@iasfbo.inaf.it>
2014-07-31 15:11:49 +0200

    first commit

---
+++ www/js/gloria_widget.js
@@ -0,0 +1,11 @@
+//window.addEventListener("load", function(){
+
+    //alert("Abraacadabra!");
+
+function load_gloria_widget(){
+
+    var xd1 = new xdone();
+    xd1.xdone_init("http://sadira.iasfbo.inaf.it/gloria-widget/XD-1/");
+
+}
+
